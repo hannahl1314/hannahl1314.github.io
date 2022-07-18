d23b1cdd118d0fd6e1a5ef3cc80811274a5262d8
hannahl123 <readlife2000@gmail.com>
2022-07-17 20:24:11 -0400

    Fixed menu

---
+++ script.js
@@ -0,0 +1,13 @@
+//hamburger
+const navSlide = () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navbar = document.querySelector(".nav-bar");
+    const navLinks = document.querySelectorAll(".nav-bar li");
+    hamburger.onclick = () => {
+        navbar.classList.toggle("nav-active");
+        //hamburger animation
+        hamburger.classList.toggle("toggle");
+    }
+}
+
+window.onload = () => navSlide();   
